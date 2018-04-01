6bc930dbc245aa9941f9450cf366853992c32b19
huatoanphong <37732599+huatoanphong@users.noreply.github.com>
2018-04-01 21:26:28 +0700

    Distance

---
+++ EmguCVDemo/EmguCVDemo/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace EmguCVDemo
+{
+    partial class frmHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imgbInput = new Emgu.CV.UI.ImageBox();
+            this.txtAxPos = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDistance = new System.Windows.Forms.TextBox();
+            this.txtAyPos = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtBxPos = new System.Windows.Forms.TextBox();
+            this.txtByPos = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgbInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(810, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // imgbInput
+            // 
+            this.imgbInput.Location = new System.Drawing.Point(12, 27);
+            this.imgbInput.Name = "imgbInput";
+            this.imgbInput.Size = new System.Drawing.Size(609, 450);
+            this.imgbInput.TabIndex = 2;
+            this.imgbInput.TabStop = false;
+            this.imgbInput.MouseClick += new System.Windows.Forms.MouseEventHandler(this.imgbInput_MouseClick);
+            this.imgbInput.MouseMove += new System.Windows.Forms.MouseEventHandler(this.imgbInput_MouseMove);
+            // 
+            // txtAxPos
+            // 
+            this.txtAxPos.Location = new System.Drawing.Point(706, 330);
+            this.txtAxPos.Name = "txtAxPos";
+            this.txtAxPos.Size = new System.Drawing.Size(41, 20);
+            this.txtAxPos.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(639, 330);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Point A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(639, 380);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Point B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(627, 448);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Distance";
+            // 
+            // txtDistance
+            // 
+            this.txtDistance.Location = new System.Drawing.Point(706, 448);
+            this.txtDistance.Name = "txtDistance";
+            this.txtDistance.Size = new System.Drawing.Size(88, 20);
+            this.txtDistance.TabIndex = 8;
+            // 
+            // txtAyPos
+            // 
+            this.txtAyPos.Location = new System.Drawing.Point(753, 330);
+            this.txtAyPos.Name = "txtAyPos";
+            this.txtAyPos.Size = new System.Drawing.Size(41, 20);
+            this.txtAyPos.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(713, 314);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "X";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(713, 364);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "X";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(763, 314);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Y";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(763, 364);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 16);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Y";
+            // 
+            // txtBxPos
+            // 
+            this.txtBxPos.Location = new System.Drawing.Point(706, 380);
+            this.txtBxPos.Name = "txtBxPos";
+            this.txtBxPos.Size = new System.Drawing.Size(41, 20);
+            this.txtBxPos.TabIndex = 15;
+            // 
+            // txtByPos
+            // 
+            this.txtByPos.Location = new System.Drawing.Point(753, 380);
+            this.txtByPos.Name = "txtByPos";
+            this.txtByPos.Size = new System.Drawing.Size(41, 20);
+            this.txtByPos.TabIndex = 16;
+            // 
+            // frmHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 489);
+            this.Controls.Add(this.txtByPos);
+            this.Controls.Add(this.txtBxPos);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAyPos);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDistance);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAxPos);
+            this.Controls.Add(this.imgbInput);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmHome";
+            this.Text = "HOME";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgbInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private Emgu.CV.UI.ImageBox imgbInput;
+        private System.Windows.Forms.TextBox txtAxPos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDistance;
+        private System.Windows.Forms.TextBox txtAyPos;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtBxPos;
+        private System.Windows.Forms.TextBox txtByPos;
+    }
+}
+
---
+++ EmguCVDemo/EmguCVDemo/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace EmguCVDemo
+{
+    public partial class frmHome : Form
+    {
+        Image<Bgr, byte> _imgInput;
+        Image<Bgr, byte> imgDraw;
+        Point startPoint;
+        Point endPoint;
+        bool isSetA = false;
+        bool isSetB = false;
+        int numClick = 0;
+        int valuePointX = 0;
+        int valuePointY = 0;
+        public frmHome()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                _imgInput = new Image<Bgr, byte>(ofd.FileName);
+                imgbInput.Image = _imgInput;
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void imgbInput_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if (_imgInput == null)
+            {
+                return;
+            }
+            else
+            {
+                int offsetX = (int)(e.Location.X / imgbInput.ZoomScale);
+                int offsetY = (int)(e.Location.Y / imgbInput.ZoomScale);
+                int horizontalScrollBarValue = imgbInput.HorizontalScrollBar.Visible ? (int)imgbInput.HorizontalScrollBar.Value : 0;
+                int verticalScrollBarValue = imgbInput.VerticalScrollBar.Visible ? (int)imgbInput.VerticalScrollBar.Value : 0;
+                valuePointX = offsetX + horizontalScrollBarValue;
+                valuePointY = offsetY + verticalScrollBarValue;
+                if (isSetA == false)
+                {
+                    txtAxPos.Text = valuePointX.ToString();
+                    txtAyPos.Text = valuePointY.ToString();
+                }
+                else if (isSetB == false)
+                {
+                    txtBxPos.Text = valuePointX.ToString();
+                    txtByPos.Text = valuePointY.ToString();
+                }
+            }
+        }
+
+        private void imgbInput_MouseClick(object sender, MouseEventArgs e)
+        {
+            switch (numClick)
+            {
+                case 0:
+                    isSetA = true;
+                    imgDraw  = new Image<Bgr, byte>(_imgInput.Bitmap);
+                    startPoint = new Point(valuePointX, valuePointY);
+                    CvInvoke.Circle(imgDraw, startPoint, 1, new MCvScalar(255, 0, 0), 2, Emgu.CV.CvEnum.LineType.EightConnected, 0);
+                    numClick++;
+                    imgbInput.Image = imgDraw;
+                    break;
+                case 1:
+                    endPoint = new Point(valuePointX, valuePointY);
+                    CvInvoke.Circle(imgDraw, endPoint, 1, new MCvScalar(255, 0, 0), 2, Emgu.CV.CvEnum.LineType.EightConnected, 0);
+                    CvInvoke.Line(imgDraw, startPoint, endPoint, new MCvScalar(255, 0, 0), 1, Emgu.CV.CvEnum.LineType.EightConnected, 0);
+                    numClick = 0;
+                    txtDistance.Text = Distance().ToString();
+                    imgbInput.Image = imgDraw;
+                    isSetA = false;
+                    isSetB = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+        public double Distance()
+        {
+            int Ax = Convert.ToInt32(txtAxPos.Text);
+            int Bx = Convert.ToInt32(txtBxPos.Text);
+            int Ay = Convert.ToInt32(txtAyPos.Text);
+            int By = Convert.ToInt32(txtByPos.Text);
+
+            double tempX = Math.Pow((Math.Abs(Ax - Bx)), 2);
+            double tempY = Math.Pow((Math.Abs(Ay - By)), 2);
+            double result = Math.Sqrt(tempX + tempY);
+            return result;
+        }
+    }
+}
